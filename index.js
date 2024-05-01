514deb82839713245e0d71672c88c2d5baacd172
Tima Gixe <timagixe@protonmail.com>
2024-05-01 16:54:34 +0300

    fix: get rid of undefined on new user registration

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ bot.onText(/^\/register/, async (message) => {
 
         await bot.sendMessage(
             message.chat.id,
-            `[${message.from.username}](tg://user?id=${message.from.id}) successfully registered!`,
+            `[${message.from.username || message.from.first_name}](tg://user?id=${message.from.id}) successfully registered!`,
             {
                 parse_mode: "Markdown"
             }
