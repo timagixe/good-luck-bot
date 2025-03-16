1730f7a441aefa286cc856e69adeab6a644585b4
Tima Gixe <timagixe@protonmail.com>
2025-03-16 12:31:19 +0200

    feat: add sendMessageWithRetryAndDelay function to improve message sending reliability with delay

--- utils.js
+++ utils.js
@@ -8,3 +8,17 @@ export function isMessageFromPerson(message) {
 export function getTodayDate() {
   return new Date().toLocaleDateString("uk-UA", { timeZone: "Europe/Kyiv" });
 }
+
+async function wait(ms = 3000) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export async function sendMessageWithRetryAndDelay({
+  bot,
+  chatId,
+  message,
+  options,
+}) {
+  await wait();
+  return withRetry(() => bot.sendMessage(chatId, message, options));
+}
