6d867cc2a2e1ea7a96fbbf53570b04f666bfb282
Tima Gixe <timagixe@protonmail.com>
2025-03-16 19:55:19 +0200

    feat: add darts game functionality with winner determination and scoring logic

--- games.js
+++ games.js
@@ -19,6 +19,15 @@ function getGameTypeMessage({ gameType, user, diceResult }) {
     return `${emoji} [${user.name}](tg://user?id=${user.id}) rolled ${diceResult.dice.value}`;
   }
 
+  if (gameType === "darts") {
+    switch (diceResult.dice.value) {
+      case 1:
+        return `${emoji} [${user.name}](tg://user?id=${user.id}) missed`;
+      default:
+        return `${emoji} [${user.name}](tg://user?id=${user.id}) scores ${diceResult.dice.value}`;
+    }
+  }
+
   return `${emoji} [${user.name}](tg://user?id=${user.id}) got ${diceResult.dice.value}`;
 }
 
@@ -66,6 +75,7 @@ async function playDiceOnBehalfOfUsers({ users, bot, chatId }) {
     gameType: "dice",
   });
 
+  // value - higher is better
   const maxValue = Math.max(...usersResults.values());
 
   const winnersMap = new Map();
@@ -91,9 +101,69 @@ export async function getWinnerFromDiceGame({ users, chatId, bot }) {
       await sendMessageWithRetryAndDelay({
         bot: bot,
         chatId: chatId,
-        message: `🎲 We have a tie between ${winners
+        message: `${gameTypeToEmoji["dice"]} We have a tie between ${winners
           .map((user) => `[${user.name}](tg://user?id=${user.id})`)
-          .join(", ")}! Rolling again for them...`,
+          .join(", ")}! Playing again for them...`,
+        options: {
+          parse_mode: "Markdown",
+          disable_notification: true,
+        },
+      });
+    }
+  }
+
+  await sendMessageWithRetryAndDelay({
+    bot: bot,
+    chatId: chatId,
+    message: `🎉 And the winner is... [${winners[0].name}](tg://user?id=${winners[0].id})!`,
+    options: {
+      parse_mode: "Markdown",
+      disable_notification: true,
+    },
+  });
+
+  return winners[0];
+}
+
+async function playDartsOnBehalfOfUsers({ users, bot, chatId }) {
+  const usersResults = await playGameOnBehalfOfUsers({
+    users,
+    bot,
+    chatId,
+    gameType: "darts",
+  });
+
+  // value = 1 - missed
+  // value >= 2 - scores points
+  // value - higher is better
+  const maxValue = Math.max(...usersResults.values());
+
+  const winnersMap = new Map();
+
+  for (const [userName, value] of usersResults.entries()) {
+    if (value === maxValue) {
+      const user = users.find((u) => u.name === userName);
+
+      winnersMap.set(userName, user);
+    }
+  }
+
+  return users.filter((user) => winnersMap.has(user.name));
+}
+
+export async function getWinnerFromDartsGame({ users, chatId, bot }) {
+  let winners = users;
+
+  while (winners.length > 1) {
+    winners = await playDartsOnBehalfOfUsers({ bot, chatId, users: winners });
+
+    if (winners.length > 1) {
+      await sendMessageWithRetryAndDelay({
+        bot: bot,
+        chatId: chatId,
+        message: `${gameTypeToEmoji["darts"]} We have a tie between ${winners
+          .map((user) => `[${user.name}](tg://user?id=${user.id})`)
+          .join(", ")}! Playing again for them...`,
         options: {
           parse_mode: "Markdown",
           disable_notification: true,
