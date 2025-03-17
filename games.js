bf863bfa99396d562f4d826cb2823551c34df94e
Tima Gixe <timagixe@protonmail.com>
2025-03-17 10:37:30 +0200

    feat: add bowling game functionality with winner determination and scoring logic

--- games.js
+++ games.js
@@ -28,6 +28,10 @@ function getGameTypeMessage({ gameType, user, diceResult }) {
     }
   }
 
+  if (gameType === "bowling") {
+    return `${emoji} [${user.name}](tg://user?id=${user.id}) scores ${diceResult.dice.value}`;
+  }
+
   return `${emoji} [${user.name}](tg://user?id=${user.id}) got ${diceResult.dice.value}`;
 }
 
@@ -156,6 +160,66 @@ export async function getWinnerFromDartsGame({ users, chatId, bot }) {
 
   while (winners.length > 1) {
     winners = await playDartsOnBehalfOfUsers({ bot, chatId, users: winners });
+
+    if (winners.length > 1) {
+      await sendMessageWithRetryAndDelay({
+        bot: bot,
+        chatId: chatId,
+        message: `${gameTypeToEmoji["darts"]} We have a tie between ${winners
+          .map((user) => `[${user.name}](tg://user?id=${user.id})`)
+          .join(", ")}! Playing again for them...`,
+        options: {
+          parse_mode: "Markdown",
+          disable_notification: true,
+        },
+      });
+    }
+  }
+
+  await sendMessageWithRetryAndDelay({
+    bot: bot,
+    chatId: chatId,
+    message: `🎉 And the winner is... [${winners[0].name}](tg://user?id=${winners[0].id})!`,
+    options: {
+      parse_mode: "Markdown",
+      disable_notification: true,
+    },
+  });
+
+  return winners[0];
+}
+
+async function playBowlingOnBehalfOfUsers({ users, bot, chatId }) {
+  const usersResults = await playGameOnBehalfOfUsers({
+    users,
+    bot,
+    chatId,
+    gameType: "bowling",
+  });
+
+  // value = 1 - missed
+  // value >= 2 - scores points
+  // value - higher is better
+  const maxValue = Math.max(...usersResults.values());
+
+  const winnersMap = new Map();
+
+  for (const [userName, value] of usersResults.entries()) {
+    if (value === maxValue) {
+      const user = users.find((u) => u.name === userName);
+
+      winnersMap.set(userName, user);
+    }
+  }
+
+  return users.filter((user) => winnersMap.has(user.name));
+}
+
+export async function getWinnerFromBowlingGame({ users, chatId, bot }) {
+  let winners = users;
+
+  while (winners.length > 1) {
+    winners = await playBowlingOnBehalfOfUsers({ bot, chatId, users: winners });
 
     if (winners.length > 1) {
       await sendMessageWithRetryAndDelay({
